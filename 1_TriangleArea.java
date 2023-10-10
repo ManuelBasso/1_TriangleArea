4c4c9b660d1b06dfcdae4637aeba247a42f95a62
ManuelBasso <146716888+ManuelBasso@users.noreply.github.com>
2023-10-10 12:57:06 +0200

    Add files via upload
    add files from jDoodle

---
+++ 1_TriangleArea.java
@@ -0,0 +1,12 @@
+//Scrivere uno snippet di codice che, dato un triangolo identificato da base ed altezza ,restituisca l'area.
+
+
+public class TriangleArea {
+    public static void main(String args[]) {
+      int base = 5;
+      int altezza = 10;
+      int area = (base * altezza)/2;
+
+      System.out.println("The area is : " + area);
+    }
+}
